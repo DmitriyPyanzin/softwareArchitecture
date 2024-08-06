16b2829c54914dd279fd94864870111d3df50b65
Dmitry_Pyanzin <lysy_41@mail.ru>
2024-08-06 18:36:32 +0300

    StartPoligonModel

--- src/hw1/modelElements/Texture.java
+++ src/hw1/modelElements/Texture.java
@@ -1,4 +1,23 @@
 package hw1.modelElements;
 
 public class Texture {
+
+    private int id;
+    private final String name;
+
+    public int getId() {
+        return id;
+
+    }
+
+    public String getName() {
+        return name;
+
+    }
+
+    public Texture(String name) {
+        this.name = name;
+
+    }
+
 }
