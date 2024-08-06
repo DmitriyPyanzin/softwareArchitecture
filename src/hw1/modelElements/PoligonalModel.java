1d052df20f28ef4adff7af9158406ecf9b6ef66b
Dmitry_Pyanzin <lysy_41@mail.ru>
2024-08-06 19:17:43 +0300

    StartFlash

--- src/hw1/modelElements/PoligonalModel.java
+++ src/hw1/modelElements/PoligonalModel.java
@@ -1,4 +1,30 @@
 package hw1.modelElements;
 
+import java.util.List;
+
 public class PoligonalModel {
+
+    private List<Poligon> poligons;
+    private List<Texture> textures;
+
+    public List<Poligon> getPoligons() {
+        return poligons;
+
+    }
+
+    public List<Texture> getTextures() {
+        return textures;
+
+    }
+
+    public PoligonalModel(List<Poligon> poligons) {
+        this.poligons = poligons;
+
+    }
+
+    public PoligonalModel(List<Poligon> poligons, List<Texture> textures) {
+        this.poligons = poligons;
+        this.textures = textures;
+    }
+
 }
